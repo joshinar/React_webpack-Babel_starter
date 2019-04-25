0ef7c6e33770270f0513617320c1334c9880df8a
Narendra Joshi <joshinaren93@gmail.com>
2019-04-25 23:39:26 +0530

    first-commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>React Boilerplate</h1>
+				<p>React webpack and babel starter created from scratch</p>
+				<p>
+					run command <code>npm install</code> to use this boilerplate in order to create an React App from
+					scratch without all junk.
+				</p>
+			</div>
+		);
+	}
+}
+export default App;
